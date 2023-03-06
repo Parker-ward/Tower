de8b400e7e19d9a132321302712500d6faf941e6
Parker <ward.parker26@gmail.com>
2023-03-05 21:33:40 -0700

    done

--- Tower.client/src/services/TicketsService.js
+++ Tower.client/src/services/TicketsService.js
@@ -12,14 +12,6 @@ class TicketsService {
     AppState.tickets = res.data.map(t => new Ticket(t))
   }
 
-  // async getMyTickets(eventId) {
-  //   AppState.myEvents = []
-  //   const res = await api.get('api/event/' + eventId + '/ticket')
-  //   logger.log('getting tickets', res.data)
-  //   AppState.myEvents = res.data.map(m => new myEvents(m))
-  // }
-
-
   async attendEvent(eventData) {
     const res = await api.post('api/tickets', eventData)
     logger.log('getting a ticket', res.data)
